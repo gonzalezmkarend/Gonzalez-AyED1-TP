3f26a94d9da48a9cba7291753bafddfd14798e67
gonzalezmkarend <gonzalez.mendez.k.d@gmail.com>
2026-09-15 11:24:38 -0300

    ej6 tp1 resuelto a lo intro algoritmia

--- ejercicios/tp01_funciones/tp01_ej07_dia_siguiente.py
+++ ejercicios/tp01_funciones/tp01_ej07_dia_siguiente.py
@@ -1,4 +1,39 @@
-''''''
+'''Escribir una función diasiguiente(dia, mes año) que reciba como parámetro una
+fecha cualquiera expresada por tres enteros y calcule y devuelva otros tres enteros
+correspondientes el día siguiente al dado. Utilizando esta función sin modificaciones
+ni agregados, desarrollar programas que permitan:
+a. Sumar N días a una fecha.
+b. Calcular la cantidad de días existentes entre dos fechas cualesquiera.'''
+
+def verifica_validez(dia = int, mes = int, anio = int) -> bool:
+    '''
+    Verifica la validez de la fecha en conjunto.
+    precondiciones = los datos deben ser enteros positivos.
+    postcondiciones = si la fecha es válida devuelve True, en caso contrario devuelve False.
+
+    '''
+    valido = True
+
+    #si el mes es febrero, verifico primero si el año es bisiesto.
+    bisiesto = False
+    if mes == 2:
+        if anio % 4 == 0 and anio % 100 != 0:
+            bisiesto = True
+
+        elif anio % 400 == 0:
+            bisiesto = True
+
+        if bisiesto == True and dia > 29:
+            valido = False
+
+        elif dia > 28:
+            valido = False
+
+    elif mes == 4 or mes == 6 or mes == 9 or mes == 11:
+        if dia > 30:
+            valido = False
+
+    return valido
 
 def main() -> None:
     pass
--- ejercicios/tp01_funciones/tp01_ej09_cajones_naranjas.py
+++ ejercicios/tp01_funciones/tp01_ej09_cajones_naranjas.py
@@ -1,4 +1,16 @@
-''''''
+'''Resolver el siguiente problema utilizando funciones:
+Un productor frutihortícola desea contabilizar sus cajones de naranjas según el peso
+para poder cargar los camiones de reparto. La empresa cuenta con N camiones, y
+cada uno puede transportar hasta media tonelada (500 kilogramos). En un cajón
+caben 100 naranjas con un peso de entre 200 y 300 gramos cada una. Si el peso
+de alguna naranja se encuentra fuera del rango indicado se la clasifica para
+procesar como jugo. Desarrollar un programa para ingresar la cantidad de naranjas
+cosechadas e informar cuántos cajones se pueden llenar, cuántas naranjas son para
+jugo y si hay algún sobrante de naranjas que deba considerarse para el siguiente
+reparto. Simular el peso de cada unidad generando un número entero al azar entre
+150 y 350.
+Además, se desea saber cuántos camiones se necesitan para transportar la cosecha, considerando que la ocupación del camión no debe ser inferior al 80%; en
+caso contrario el camión no serán despachado por su alto costo. '''
 
 def main() -> None:
     pass
